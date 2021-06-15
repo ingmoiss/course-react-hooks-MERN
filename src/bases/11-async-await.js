97276a2ca6cd839ac17f9aeaf094e4f48a4ece43
Moisés Navarro <moi55navacor@gmail.com>
2021-06-14 23:30:38 -0600

    Added Async and Await

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,20 @@
+
+const getImagen = async() => {
+    
+    try{
+        const apiKey = 'MNS8ajZAjcsueu8YkcUP5omMEjTrhiiR';
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const { data } = await resp.json();
+    
+        const { url } = data.images.original;
+    
+        const img = document.createElement('img');
+        img.src = url;
+    
+        document.body.append(img);
+    }catch(error){
+        console.log(error);
+    }
+
+
+}
